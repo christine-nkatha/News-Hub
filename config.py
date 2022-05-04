1dbbeae4fcd022526ddac5e2a17d9bc232df545d
christine-nkatha <nkathachristine456@gmail.com>
2022-05-04 13:50:23 +0300

    modified my congig.py

---
+++ config.py
@@ -0,0 +1,4 @@
+import os 
+class config:
+    NEWS_API_BASE_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey=190cc6c8413944bfa784e6567eafc577'
+    NEWS_API_KEY=os.environ.get('190cc6c8413944bfa784e6567eafc577')
