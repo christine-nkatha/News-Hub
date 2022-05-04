5ff226a8bf56858950025233c72ede6a81c218b9
christine-nkatha <nkathachristine456@gmail.com>
2022-05-04 14:31:20 +0300

    debuging

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from distutils.debug import DEBUG
 import os
 class config:
     NEWS_API_BASE_URL='https://newsapi.org/v2/top-headlines?country=us&apiKey=190cc6c8413944bfa784e6567eafc577'
@@ -8,10 +9,10 @@ class prodconfig(config):
     pass
 
 class DevConfig(config):
-    Debug=True
+    DEBUG = True
 
 
-    config_options={
+config_options={
         'development':DevConfig,
         'production':prodconfig
 
