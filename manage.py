7d9ab566cd3749ada3e66b04dd01e56300157d4f
christine-nkatha <nkathachristine456@gmail.com>
2022-05-04 17:58:45 +0300

    debugging

--- manage.py
+++ manage.py
@@ -1,11 +1,20 @@
 from app import create_app
-from flask_script import manager,server 
-  
-app =create_app('development')
+from flask_script import Manager,Server
 
-manager=manager(app);
-manager.add_command('server',server) 
+# Creating app instance
+app = create_app('development')
 
-  
-if __name__ =='__main__':
-     manager.run()
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+if __name__ == '__main__':
+    manager.run()
---
+++ start.sh
@@ -0,0 +1,4 @@
+export NEWS_API_KEY='190cc6c8413944bfa784e6567eafc577'
+
+
+python3.9 manage.py server
